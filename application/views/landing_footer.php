59b3e691eb1bb5a9b375900f596e5160fa188e90
mizdris <ciureamihail@outlook.com>
2017-05-27 18:27:49 +0300

    assets

--- application/views/landing_footer.php
+++ application/views/landing_footer.php
@@ -5,3 +5,16 @@
  * Date: 27.05.2017
  * Time: 18:14
  */
+?>
+</div>
+</div>
+<!-- /#wrapper -->
+<!-- jQuery -->
+<script src="<?php echo ASSETS; ?>plugins/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="<?php echo ASSETS; ?>bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- jQuery for carousel -->
+<script src="<?php echo ASSETS; ?>plugins/bower_components/owl.carousel/owl.carousel.min.js"></script>
+<script src="<?php echo ASSETS; ?>landing/js/custom.js"></script>
+</body>
+</html>
--- application/views/landing_header.php
+++ application/views/landing_header.php
@@ -5,3 +5,38 @@
  * Date: 27.05.2017
  * Time: 18:14
  */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo ASSETS; ?>plugins/images/favicon.png">
+    <title><?php echo $page_title; ?></title>
+    <!-- Bootstrap Core CSS -->
+    <link href="<?php echo ASSETS; ?>bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!--My admin Custom CSS -->
+    <link href="<?php echo ASSETS; ?>plugins/bower_components/owl.carousel/owl.carousel.min.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo ASSETS; ?>plugins/bower_components/owl.carousel/owl.theme.default.css" rel="stylesheet" type="text/css"/>
+    <!-- animation CSS -->
+    <link href="<?php echo ASSETS; ?>css/animate.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="<?php echo ASSETS; ?>landing/css/style.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body class="">
+<!-- Preloader -->
+
+<div id="wrapper">
+    <div class="container-fluid">  
