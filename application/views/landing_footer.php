5e0ffca9e88ea2bbb946fa82524cd43163c77802
VadeRetro <gabi@digitaladvisors.com>
2017-05-27 22:51:11 +0300

    modal login

--- application/views/landing_footer.php
+++ application/views/landing_footer.php
@@ -230,11 +230,11 @@
         $("#creeaza_cont").click(function (e) {
             e.preventDefault();
             var nume = $('#nume').val();
-            var prenume = $('#prenume').text();
+            var prenume = $('#prenume').val();
             var email = $('#register_email').val();
             var pass = $('#register_parola').val();
             var repass = $('#register_reparola').val();
-            alert(nume);
+            alert(nume.length());
             if (nume == '') {
                 $(this).css({'background-color': '#fb9678', 'color': 'white'});
                 $('#form_errors').html('Numele este obligatoriu!').removeClass('hidden');
