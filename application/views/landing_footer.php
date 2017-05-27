939a813f7bef924c4e58b18b051d58d9374c1383
VadeRetro <gabi@digitaladvisors.com>
2017-05-27 23:16:55 +0300

    modal login

--- application/views/landing_footer.php
+++ application/views/landing_footer.php
@@ -250,7 +250,8 @@
             if (pass.length === 0) {
                 $("#register_parola").css({'background-color': '#fb9678', 'color': 'white'});
                 $('#form_errors').append('<p>Nu ai introdus parola!</p>').removeClass('hidden');
-            } else if (pass.length < 7) {
+            }
+            if (pass.length < 7) {
                 $("#register_parola").css({'background-color': '#fb9678', 'color': 'white'});
                 $('#form_errors').append('<p>Minim 6 caractere!</p>').removeClass('hidden');
             }
