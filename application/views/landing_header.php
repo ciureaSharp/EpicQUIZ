7969d863c8957de1bf4e609c891b2dde7f405d4c
mizdris <ciureamihail@outlook.com>
2017-05-27 18:48:26 +0300

    assets

--- application/views/landing_header.php
+++ application/views/landing_header.php
@@ -23,7 +23,7 @@
     <link href="<?php echo ASSETS; ?>plugins/bower_components/owl.carousel/owl.carousel.min.css" rel="stylesheet" type="text/css"/>
     <link href="<?php echo ASSETS; ?>plugins/bower_components/owl.carousel/owl.theme.default.css" rel="stylesheet" type="text/css"/>
     <!-- animation CSS -->
-    <link href="<?php echo ASSETS; ?>css/animate.css" rel="stylesheet">
+    <link href="<?php echo ASSETS; ?>landing/css/animate.css" rel="stylesheet">
     <!-- Custom CSS -->
     <link href="<?php echo ASSETS; ?>landing/css/style.css" rel="stylesheet">
 
