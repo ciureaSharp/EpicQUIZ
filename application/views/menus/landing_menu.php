5e170a2e758ef8aa696f440e2765dee7a72f0ab7
mizdris <ciureamihail@outlook.com>
2017-05-27 18:08:58 +0300

    assets

--- application/views/menus/landing_menu.php
+++ application/views/menus/landing_menu.php
@@ -7,4 +7,51 @@
  */
 ?>
 
+<!-- Row -->
+<div class="row">
+    <div class="col-md-12 navbar-fixed">
+        <div class="fix-width">
+            <nav class="navbar navbar-default">
 
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#"><img src="images/elite-admin-logo.png" alt="Eliteadmin" /></a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+
+                    <ul class="nav navbar-nav custom-nav navbar-right">
+
+                        <li class="dropdown cst-dwn">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ELITE DEMOS <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin/index.html" target="_blank">Eliteadmin</a></li>
+                                <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/index.html" target="_blank">Eliteadmin Inverse</a></li>
+                                <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-mini-sidebar/index3.html" target="_blank">Eliteadmin Dark</a></li>
+                                <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-mini-sidebar/index3.html" target="_blank">Eliteadmin Mini Sidebar</a></li>
+                                <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-iconbar/index4.html" target="_blank">Eliteadmin Iconbar</a></li>
+                                <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-ecommerce/index.html" target="_blank">Eliteadmin eCommerce</a></li>
+                                <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-horizontal-navbar/index3.html" target="_blank">Horizontal nav & Box</a></li>
+                                <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-horizontal-nav-fullwidth/index.html" target="_blank">Horizontal nav & Fullwidth</a></li>
+                                <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-rtl/index.html" target="_blank">Eliteadmin RTL</a></li>
+                                <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse-rtl/index.html" target="_blank">Inverse RTL</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSfw19GviedvGupnnHt1Uqbwgy_AnvFy7NEoY2ZEfoZUW9koTg/viewform" target="_blank">SUGGEST FEATURES</a></li>
+                        <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/documentation.html" target="_blank">DOCUMENTATION</a></li>
+                        <li><a href="https://themeforest.net/item/elite-admin-responsive-web-app-kit-/16750820?clickthrough_id= 16750820&license=regular&open_purchase_for_item_id= 16750820&purchasable=source&redirect_back=true&ref=suniljoshi&utm_source=item_desc_link" class="btn btn-danger btn-rounded custom-btn" target="_blank">BUY NOW</a></li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </nav>
+        </div>
+    </div>
+</div>
+<!-- /Row -->
