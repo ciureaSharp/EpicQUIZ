e837f9bf8af87f8e799557d15800a441503d38d5
VadeRetro <gabi@digitaladvisors.com>
2017-05-27 20:12:51 +0300

    modal login

--- application/views/landing_footer.php
+++ application/views/landing_footer.php
@@ -16,7 +16,7 @@
             </div>
             <div class="modal-body">
                 <div class="row">
-                    <div class="col-md-8"  style="border-right: 3px solid #EEEEEE">
+                    <div class="col-md-8"  style="border-right: 3px solid #838ea5">
                         <form class="form-horizontal">
                             <div class="form-group">
                                 <label for="exampleInputEmail1" class="col-sm-3 control-label">Email*</label>
