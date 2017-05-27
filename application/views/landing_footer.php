7591f385467d7279ade45385311ad46385be657a
mizdris <ciureamihail@outlook.com>
2017-05-27 19:29:10 +0300

    assets

--- application/views/landing_footer.php
+++ application/views/landing_footer.php
@@ -10,35 +10,47 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="exampleModalLabel1">New message</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                        aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="Login_modalLabel1">Bine ai venit!</h4>
             </div>
             <div class="modal-body">
-                <form class="form-horizontal">
-                    <div class="form-group">
-                        <label for="exampleInputEmail1" class="col-sm-3 control-label">Email*</label>
-                        <div class="col-sm-9">
-                            <div class="input-group">
-                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                                <div class="input-group-addon"><i class="ti-email"></i></div>
+                <div class="row">
+                    <form class="form-horizontal">
+                        <div class="col-md-5">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1" class="col-sm-3 control-label">Email*</label>
+                                <div class="col-sm-9">
+                                    <div class="input-group">
+                                        <input type="email" class="form-control" id="exampleInputEmail1"
+                                               placeholder="Enter email">
+                                        <div class="input-group-addon"><i class="ti-email"></i></div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="inputPassword3" class="col-sm-3 control-label">Password*</label>
-                        <div class="col-sm-9">
-                            <div class="input-group">
-                                <input type="password" class="form-control" id="exampleInputpwd2" placeholder="Enter pwd">
-                                <div class="input-group-addon"><i class="ti-lock"></i></div>
+                        <div class="col-md-5">
+                            <div class="form-group">
+                                <label for="inputPassword3" class="col-sm-3 control-label">Password*</label>
+                                <div class="col-sm-9">
+                                    <div class="input-group">
+                                        <input type="password" class="form-control" id="exampleInputpwd2"
+                                               placeholder="Enter pwd">
+                                        <div class="input-group-addon"><i class="ti-lock"></i></div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="form-group m-b-0">
-                        <div class="col-sm-offset-3 col-sm-9 text-right">
-                            <button type="submit" class="btn btn-info waves-effect waves-light m-t-10">Sign in</button>
+                        <div class="col-md-2">
+                            <div class="form-group m-b-0">
+                                <div class="col-sm-offset-3 col-sm-9 text-right">
+                                    <button type="submit" class="btn btn-info waves-effect waves-light m-t-10">Sign in
+                                    </button>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
@@ -47,32 +59,52 @@
 <footer class="footer">
     <div class="row">
         <div class="fix-width">
-            <div class="col-md-3 col-sm-6"><img src="<?php echo ASSETS; ?>landing/images/footer-logo.png"/><p class="m-t-30"><font class="text-white">Eliteadmin</font> is premium quality admin dashboard template with flat design. It is fully responsive admin dashboard template built with Bootstrap Framework, HTML5 & CSS3, Media query. </p></div>
+            <div class="col-md-3 col-sm-6"><img src="<?php echo ASSETS; ?>landing/images/footer-logo.png"/>
+                <p class="m-t-30"><font class="text-white">Eliteadmin</font> is premium quality admin dashboard template
+                    with flat design. It is fully responsive admin dashboard template built with Bootstrap Framework,
+                    HTML5 & CSS3, Media query. </p></div>
             <div class="col-md-3 col-sm-6">
                 <ul class="footer-link list-icons">
-                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/index.html"><i class="ti-angle-right text-megna"></i> Minimal Dashboard</a></li>
-                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/index2.html"><i class="ti-angle-right text-megna"></i> Demographical Dashboad</a></li>
-                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/index3.html"><i class="ti-angle-right text-megna"></i> Analytical Dashboad</a></li>
-                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/index4.html"><i class="ti-angle-right text-megna"></i> Simpler Dashboard</a></li>
-                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-ecommerce/index.html"><i class="ti-angle-right text-megna"></i> eCommerce Dashboard</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/index.html"><i
+                                class="ti-angle-right text-megna"></i> Minimal Dashboard</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/index2.html"><i
+                                class="ti-angle-right text-megna"></i> Demographical Dashboad</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/index3.html"><i
+                                class="ti-angle-right text-megna"></i> Analytical Dashboad</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/index4.html"><i
+                                class="ti-angle-right text-megna"></i> Simpler Dashboard</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-ecommerce/index.html"><i
+                                class="ti-angle-right text-megna"></i> eCommerce Dashboard</a></li>
                 </ul>
             </div>
             <div class="col-md-3 col-sm-6">
                 <ul class="footer-link list-icons">
-                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/widgets.html"><i class="ti-angle-right text-megna"></i> Widget Sections</a></li>
-                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/morris-chart.html"><i class="ti-angle-right text-megna"></i> Chart Options</a></li>
-                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/form-layout.html"><i class="ti-angle-right text-megna"></i> Lots of Form Layouts</a></li>
-                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/data-table.html"><i class="ti-angle-right text-megna"></i> Data Table Options</a></li>
-                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/calendar.html"><i class="ti-angle-right text-megna"></i> Calendar Designs</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/widgets.html"><i
+                                class="ti-angle-right text-megna"></i> Widget Sections</a></li>
+                    <li>
+                        <a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/morris-chart.html"><i
+                                class="ti-angle-right text-megna"></i> Chart Options</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/form-layout.html"><i
+                                class="ti-angle-right text-megna"></i> Lots of Form Layouts</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/data-table.html"><i
+                                class="ti-angle-right text-megna"></i> Data Table Options</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/calendar.html"><i
+                                class="ti-angle-right text-megna"></i> Calendar Designs</a></li>
                 </ul>
             </div>
             <div class="col-md-3 col-sm-6">
                 <ul class="footer-link list-icons">
-                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/inbox.html"><i class="ti-angle-right text-megna"></i> Inbox Layout</a></li>
-                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/treeview.html"><i class="ti-angle-right text-megna"></i> Tree View Options</a></li>
-                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/carousel.html"><i class="ti-angle-right text-megna"></i> Carousel Slider Option</a></li>
-                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/gallery.html"><i class="ti-angle-right text-megna"></i> Gallery Option</a></li>
-                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/starter-page.html"><i class="ti-angle-right text-megna"></i> Starter Pages</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/inbox.html"><i
+                                class="ti-angle-right text-megna"></i> Inbox Layout</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/treeview.html"><i
+                                class="ti-angle-right text-megna"></i> Tree View Options</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/carousel.html"><i
+                                class="ti-angle-right text-megna"></i> Carousel Slider Option</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/gallery.html"><i
+                                class="ti-angle-right text-megna"></i> Gallery Option</a></li>
+                    <li>
+                        <a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/starter-page.html"><i
+                                class="ti-angle-right text-megna"></i> Starter Pages</a></li>
                 </ul>
             </div>
             <div class="clearfix"></div>
