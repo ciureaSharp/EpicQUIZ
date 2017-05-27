ff1ed4d0f36c105b2152c31b0b2969e315f5730b
mizdris <ciureamihail@outlook.com>
2017-05-27 18:46:35 +0300

    assets

--- application/views/landing_footer.php
+++ application/views/landing_footer.php
@@ -6,6 +6,47 @@
  * Time: 18:14
  */
 ?>
+<footer class="footer">
+    <div class="row">
+        <div class="fix-width">
+            <div class="col-md-3 col-sm-6"><img src="<?php echo ASSETS; ?>landing/images/footer-logo.png"/><p class="m-t-30"><font class="text-white">Eliteadmin</font> is premium quality admin dashboard template with flat design. It is fully responsive admin dashboard template built with Bootstrap Framework, HTML5 & CSS3, Media query. </p></div>
+            <div class="col-md-3 col-sm-6">
+                <ul class="footer-link list-icons">
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/index.html"><i class="ti-angle-right text-megna"></i> Minimal Dashboard</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/index2.html"><i class="ti-angle-right text-megna"></i> Demographical Dashboad</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/index3.html"><i class="ti-angle-right text-megna"></i> Analytical Dashboad</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/index4.html"><i class="ti-angle-right text-megna"></i> Simpler Dashboard</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-ecommerce/index.html"><i class="ti-angle-right text-megna"></i> eCommerce Dashboard</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <ul class="footer-link list-icons">
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/widgets.html"><i class="ti-angle-right text-megna"></i> Widget Sections</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/morris-chart.html"><i class="ti-angle-right text-megna"></i> Chart Options</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/form-layout.html"><i class="ti-angle-right text-megna"></i> Lots of Form Layouts</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/data-table.html"><i class="ti-angle-right text-megna"></i> Data Table Options</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/calendar.html"><i class="ti-angle-right text-megna"></i> Calendar Designs</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <ul class="footer-link list-icons">
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/inbox.html"><i class="ti-angle-right text-megna"></i> Inbox Layout</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/treeview.html"><i class="ti-angle-right text-megna"></i> Tree View Options</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/carousel.html"><i class="ti-angle-right text-megna"></i> Carousel Slider Option</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/gallery.html"><i class="ti-angle-right text-megna"></i> Gallery Option</a></li>
+                    <li><a href="http://themedesigner.in/demo/eliteadmin-package/eliteadmin-inverse/starter-page.html"><i class="ti-angle-right text-megna"></i> Starter Pages</a></li>
+                </ul>
+            </div>
+            <div class="clearfix"></div>
+
+            <div class="col-md-12 sub-footer">
+                <span>Copyright 2016. All Rights Reserved by <font class="text-white">Elite Admin</font></span>
+                <span class="pull-right">Design & Developed by <font class="text-white">Theme Designer</font></span>
+            </div>
+
+        </div>
+    </div>
+</footer>
 </div>
 </div>
 <!-- /#wrapper -->
