fb1af8b62d6052178ec8e5675fae27141a0f6e46
Mihai Ciurea <ciureamihail@outlook.com>
2017-06-11 09:51:14 +0300

    genereaza link activare

--- application/models/Users_model.php
+++ application/models/Users_model.php
@@ -80,24 +80,10 @@ Class Users_model extends CI_Model
             return false;
         }
     }
-//
-//// Read data from database to show data in admin page
-//    public function read_user_information($username) {
-//
-//        $condition = "email=" . "'" . $username . "'";
-//        $this->db->select('*');
-//        $this->db->from('operatori');
-//        $this->db->where($condition);
-//        $this->db->limit(1);
-//        $query = $this->db->get();
-//
-//        if ($query->num_rows() == 1) {
-//            return $query->result();
-//        } else {
-//            return false;
-//        }
-//    }
 
+    public function generate_link(){
+        
+    }
 }
 
 
